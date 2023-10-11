fe0e51a41b9b3b679157d335f378ce0634f11612
Bach <jiramethsitasathien@gmail.com>
2023-10-11 16:54:50 +0700

    Profile Picture

--- src/client/scenes/1-gate.js
+++ src/client/scenes/1-gate.js
@@ -1,5 +1,9 @@
 import Swal from 'sweetalert2';
 
+import avatar_1 from '../assets/Profile1.jpg';
+import avatar_2 from '../assets/Profile2.jpg';
+import avatar_3 from '../assets/Profile3.jpg';
+import avatar_4 from '../assets/Profile4.jpg';
 import { socket } from '../socket';
 import { store } from '../state';
 import { setupLobby } from './2-lobby';
@@ -15,11 +19,17 @@ export function setupGate() {
   });
 
   document.querySelector('#app').innerHTML = `
-      <div class="flex flex-col h-full justify-center items-center gap-2">
-        <input id="input-name" type="text" placeholder="Nickname" class="w-96 h-14 p-4 border-2 focus:border-gray-600 border-gray-500 rounded-full outline-none" />
-        <button id="button-enter" class="w-96 h-10 bg-gray-500 hover:bg-gray-600 text-white rounded-full">Enter</button>
-      </div>
-    `;
+  <div class="flex flex-col h-full justify-center items-center gap-2">
+    <div class="avatar-container">
+      <img src=${avatar_1} class="avatar" id="avatar" />
+      <button id="prev-avatar" class="btnl">&#8592;</button>
+      <button id="next-avatar" class="btnr">&#8594;</button>
+    </div>
+    <input id="input-name" type="text" placeholder="Nickname" class="w-96 h-14 p-4 border-2 focus-border-gray-600 border-gray-500 rounded-full outline-none" />
+    <button id="button-enter" class="w-96 h-10 bg-gray-500 hover-bg-gray-600 text-white rounded-full">Enter</button>
+  </div>
+`;
+
   /* Can put your nickname when the game starts. */
   document.querySelector('#button-enter').addEventListener('click', () => {
     const name = document.querySelector('#input-name').value;
@@ -67,4 +77,30 @@ export function setupGate() {
   // Add an event listener to the button
   const darkModeButton = document.getElementById('dark-mode-toggle-button');
   darkModeButton.addEventListener('click', toggleDarkMode);
+
+  // JavaScript code to handle avatar navigation
+  const avatars = [avatar_1, avatar_2, avatar_3, avatar_4];
+  let currentAvatarIndex = 0;
+  const avatarElement = document.getElementById('avatar');
+
+  // Function to update the displayed avatar
+  function updateAvatar() {
+    avatarElement.src = avatars[currentAvatarIndex];
+  }
+
+  // Event listeners for previous and next buttons
+  const prevAvatarButton = document.getElementById('prev-avatar');
+  prevAvatarButton.addEventListener('click', () => {
+    currentAvatarIndex = (currentAvatarIndex - 1 + avatars.length) % avatars.length;
+    updateAvatar();
+  });
+
+  const nextAvatarButton = document.getElementById('next-avatar');
+  nextAvatarButton.addEventListener('click', () => {
+    currentAvatarIndex = (currentAvatarIndex + 1) % avatars.length;
+    updateAvatar();
+  });
+
+  // Initialize the displayed avatar
+  updateAvatar();
 }

d90b68c58b7e005b30b960cce41c1049d0bdf00d
ts <ts.titang@gmail.com>
2023-10-14 14:21:15 +0700

    Level description

--- src/client/scenes/2-lobby.js
+++ src/client/scenes/2-lobby.js
@@ -13,7 +13,7 @@ export function setupLobby(rooms) {
         const roomEl = document.createElement('div');
         roomEl.innerHTML = `
             <button class="flex justify-between items-center w-96 h-14 p-4 border-2 bg-gray-200 rounded-full">
-              <p>${room.name}</p>
+            <p>${room.name}</p>
               <p> ${
                 room.users.some((user) => user.id === socket.id)
                   ? 'Waiting for other player'
@@ -69,12 +69,19 @@ export function setupLobby(rooms) {
           <div id="rooms" class="flex flex-col gap-2"></div>
           <div class="flex flex-col gap-1" id="form-create-room">
             <input id="input-room-name" type="text" placeholder="Room Name" class="w-96 h-12 p-4 border-2 focus:border-gray-600 border-gray-500 rounded-full outline-none" />
-          
+            
+            <button id="hint-info" class="fixed bottom-16 left-4 bg-gray-500 hover:bg-gray-600 text-white rounded-full px-5 py-2">Level description</button>
+            <div class="fixed bottom-4 left-4 grid gap-2 grid-cols-3 text-white rounded-full">
+              <button id="hint-info1" class="bg-gray-500 hover:bg-gray-600 rounded-full px-flex py-2">Lv.1</button>
+              <button id="hint-info2" class="bg-gray-500 hover:bg-gray-600 rounded-full px-3 py-2">Lv.2</button>
+              <button id="hint-info3" class="bg-gray-500 hover:bg-gray-600 rounded-full px-3 py-2">Lv.3</button>
+            </div>
+
             <button class="w-96 h-10 bg-gray-500 hover:bg-gray-600 text-white rounded-full">Select level :
               <select id="select1" class="bg-gray-500 hover:bg-gray-600 text-white">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
+                <option value="1">Lv.1 Beginner</option>
+                <option value="2">Lv.2 Intermediate</option>
+                <option value="3">Lv.3 Advanced</option>
               </select>
             </button>
  
@@ -93,6 +100,42 @@ export function setupLobby(rooms) {
         </div>
       </div>
     `;
+  document.querySelector('#hint-info').addEventListener('click', () => {
+    Swal.fire({
+      icon: 'info',
+      title: 'Description',
+      text: 'Select level dropdown is made to choose how helpful the hint would be',
+    });
+    return;
+  });
+
+  document.querySelector('#hint-info1').addEventListener('click', () => {
+    Swal.fire({
+      icon: 'info',
+      title: 'Lv.1 Beginner',
+      text: 'Hint with no penalty',
+    });
+    return;
+  });
+
+  document.querySelector('#hint-info2').addEventListener('click', () => {
+    Swal.fire({
+      icon: 'info',
+      title: 'Lv.2 Intermediate',
+      text: 'Hint will not increase score',
+    });
+    return;
+  });
+
+  document.querySelector('#hint-info3').addEventListener('click', () => {
+    Swal.fire({
+      icon: 'info',
+      title: 'Lv.3 Advanced',
+      text: 'No hint',
+    });
+    return;
+  });
+
   updateRooms(rooms);
   document.querySelector('#button-create-room').addEventListener('click', () => {
     const roomName = document.querySelector('#input-room-name').value;

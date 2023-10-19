01e13e99ef979ed5fdfe42438426b948ac25175f
kmkm <rujapa.was@gmail.com>
2023-10-19 21:45:09 +0700

    elevator/ break add color to button

--- apps/client/src/scenes/Elevator.tsx
+++ apps/client/src/scenes/Elevator.tsx
@@ -7,14 +7,33 @@ export function Elevator() {
   const onClickReady = () => {
     socket.emit('ready');
   };
+  // const myStyle = {
+  //   backgroundImage:
+  //     "url('https://www.google.com/url?sa=i&url=https%3A%2F%2Foutrightgames.com%2Fus%2Fgames%2Fmy-friend-peppa-pig%2F&psig=AOvVaw1sC4VxjIbNacmhsFJ2c_N1&ust=1697812579140000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIDmvO-qgoIDFQAAAAAdAAAAABAE')",
+  // };
 
   const otherPlayerIndex = myPlayerIndex === 1 ? 0 : 1;
-
+  //style={{ backgroundImage:`url(${image})` }}
   return (
-    <div>
-      <p>Other player is {myRoom?.ready[otherPlayerIndex] ? 'Ready' : 'Not Ready'}</p>
-      <p>Are you ready?</p>
-      <button onClick={onClickReady}>Yes</button>
+    // <div style={{ backgroundImage: 'url(/break.png)' }}>
+    <div className=" mt-40">
+      <p className="w-fit mx-auto ">
+        <span className="flex justify-center bg-blue-500 text-white font-bold py-2 px-6 rounded-full text-2xl">
+          <span className=" mr-2 place-content-start bg-white text-black font-bold py-1 px-6 rounded-full ">
+            {myRoom?.level}
+          </span>{' '}
+          : <span className="ml-2">{myRoom?.name}</span>
+        </span>
+      </p>
+      <p className="max-w-2xl mx-auto flex justify-center text-6xl mt-10">
+        Other player is {myRoom?.ready[otherPlayerIndex] ? 'Ready' : 'Not Ready'}
+      </p>
+
+      <p className="max-w-2xl mx-auto flex justify-center text-2xl mt-10 ">Are you ready?</p>
+      <div className="max-w-[15%] mx-auto flex justify-center bg-red-500 hover:bg-red-700 text-white text-6xl font-bold rounded-full py-4 px-8 mt-10">
+        <button onClick={onClickReady}>Ready</button>
+      </div>
     </div>
+    // </div>
   );
 }
